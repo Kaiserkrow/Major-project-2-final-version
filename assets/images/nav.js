562295a6940aa92055e8d14da891ff4b64dd5615
Kaiserkrow <42108060+Kaiserkrow@users.noreply.github.com>
2022-09-19 16:05:39 +0800

    landing page v2 done

--- assets/images/nav.js
+++ assets/images/nav.js
@@ -8,8 +8,11 @@ function openNav() {
   } else {
     sideBar.style.width = "250px";
   }
+
+  document.getElementById("main").style.marginLeft = "250px";
 }
 
 function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
 }
