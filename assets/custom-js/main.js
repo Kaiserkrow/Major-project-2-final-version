f8222bccd899c0d133fed577c7fba0a23b0143d2
Kaiserkrow <42108060+Kaiserkrow@users.noreply.github.com>
2022-09-20 13:39:38 +0800

    added carousel to daily deals

--- assets/custom-js/main.js
+++ assets/custom-js/main.js
@@ -10,6 +10,37 @@ function openNav() {
   }
 }
 
+function openCart() {
+  let width = window.innerWidth;
+  let cart = document.getElementById("myCart");
+  if (width < 600) {
+    cart.style.borderTopLeftRadius = "0px";
+    cart.style.borderBottomLeftRadius = "0px";
+    cart.style.width = "100vw";
+  } else {
+    cart.style.width = "250px";
+  }
+}
 function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
 }
+
+function closeCart() {
+  document.getElementById("myCart").style.width = "0";
+}
+
+let items = document.querySelectorAll(".carousel .carousel-item");
+
+items.forEach((el) => {
+  const minPerSlide = 4;
+  let next = el.nextElementSibling;
+  for (var i = 1; i < minPerSlide; i++) {
+    if (!next) {
+      // wrap carousel by using first child
+      next = items[0];
+    }
+    let cloneChild = next.cloneNode(true);
+    el.appendChild(cloneChild.children[0]);
+    next = next.nextElementSibling;
+  }
+});
